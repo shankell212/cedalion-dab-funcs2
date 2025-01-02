85a21b8c9a6ad96d4b5a6227c73e78b61e93d9b1
David Boas <dboas@bu.edu>
2025-01-02 14:57:53 -0500

    Update DABfuncs_group_avg.py

--- DABfuncs_group_avg.py
+++ DABfuncs_group_avg.py
@@ -15,6 +15,75 @@ import numpy as np
 
 
 
+def block_average( conc_filt, stim, geo3d, trange_hrf, glm_basis_func_param, glm_drift_order, flag_do_GLM, ssr_rho_thresh, stim_lst_hrf ):
+
+    # Do GLM or Block Average
+    # Right now the GLM doesn't handle NaN's, but pruned channels have NaN's.. curious.
+    if not flag_do_GLM:
+        # do block average
+        pred_hrf = conc_filt
+
+    else:
+        # do the GLM
+
+        # get the short separation channels
+        ts_long, ts_short = cedalion.nirs.split_long_short_channels(
+            conc_filt, geo3d, distance_threshold=ssr_rho_thresh
+        )
+
+        # if stim has a column named 'amplitude' then rename it to 'value'
+        # I noticed that some events files have 'amplitude' and some have 'value' and some have neither
+        if 'amplitude' in stim.columns:
+            stim = stim.rename(columns={"amplitude": "value"})
+        elif 'amplitude' not in stim.columns and 'value' not in stim.columns:
+            stim['value'] = 1
+
+        # make the design matrix
+        # ’closest’: Use the closest short channel 
+        # ‘max_corr’: Use the short channel with the highest correlation 
+        # ‘mean’: Use the average of all short channels.
+        dm, channel_wise_regressors = glm.make_design_matrix(
+            conc_filt,
+            ts_short,
+            stim[stim.trial_type.isin(stim_lst_hrf)],
+            geo3d,
+            basis_function = glm.GaussianKernels(trange_hrf[0], trange_hrf[1], t_delta=glm_basis_func_param, t_std=glm_basis_func_param), 
+            drift_order = glm_drift_order, 
+            short_channel_method='mean'
+        )
+
+        # fit the GLM model
+        betas = glm.fit(conc_filt, dm, channel_wise_regressors, noise_model="ols")
+
+        # prediction of all HRF regressors, i.e. all regressors that start with 'HRF '
+        pred_hrf = glm.predict(
+            conc_filt,
+            betas.sel(regressor=betas.regressor.str.startswith("HRF ")),
+            dm,
+            channel_wise_regressors
+        )
+
+    # get the HRF prediction 
+    # This is a simple way to get HRF stats by getting epochs and then simple block average, 
+    # but it will not work for event related designs with overlapping epochs.
+    # In the future we will have to update glm.predict to provide HRF stats.
+    pred_hrf = pred_hrf.transpose('chromo', 'channel', 'time')
+    pred_hrf = pred_hrf.assign_coords(samples=('time', np.arange(len(pred_hrf.time))))
+    pred_hrf['time'] = pred_hrf.time.pint.quantify(units.s)     
+    pred_hrf['source'] = conc_filt.source
+    pred_hrf['detector'] = conc_filt.detector
+
+    conc_epochs_tmp = pred_hrf.cd.to_epochs(
+                                stim,  # stimulus dataframe
+                                set(stim[stim.trial_type.isin(stim_lst_hrf)].trial_type), # select events
+#                                set(stim.trial_type),  # select events
+                                before=trange_hrf[0],  # seconds before stimulus
+                                after=trange_hrf[1],  # seconds after stimulus
+                            )
+        
+    return conc_epochs_tmp
+
+
 
 
 
